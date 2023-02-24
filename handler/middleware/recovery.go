6873fbbda3e2aae90c1c3a8bd93f8014e3256e57
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2023-02-24 15:59:01 +0900

    feat: implement recovery middleware

---
+++ handler/middleware/recovery.go
@@ -0,0 +1,18 @@
+package middleware
+
+import (
+	"net/http"
+)
+
+// TODO: implement error recovery middleware
+func Recovery(h http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+			}
+		}()
+		h.ServeHTTP(w, r)
+	}
+	return http.HandlerFunc(fn)
+}
